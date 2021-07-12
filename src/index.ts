9e3b7a8f8af69788c94653255dd9d9b2627a74b2
Jorge989 <attiejorge89@gmail.com>
2021-07-12 20:44:18 -0300

    gogo

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import "reflect-metadata";
 import express from "express";
 import "./database/connect";
 import routes from "./routes";
+const cors = require("cors");
 const app = express();
-
+const PORT = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3000, () => console.log("🔥 Servidor rodando!"));
+app.listen(PORT, () => console.log("🔥 Servidor rodando!"));
